52b9f901d954342d87f2dedf5093128250fc3497
jessebanks <jesse.banks@vendscreen.com>
2017-01-17 16:24:56 -0800

    String methods to Complex type

--- app/src/main/java/wtf/jessebanks/hawthorne/dsp/Complex.kt
+++ app/src/main/java/wtf/jessebanks/hawthorne/dsp/Complex.kt
@@ -9,6 +9,19 @@ data class Complex(val real: Double, val imaginary: Double) {
     constructor() : this(0.0, 0.0)
 
     val magnitude = Math.sqrt(real * real + imaginary * imaginary)
+    val phase = Math.atan2(imaginary, real)
+
+    val rectangular: String
+        get() {
+            if (imaginary < 0) {
+                return "$real - ${-imaginary}i"
+            }
+            else {
+                return "$real + ${imaginary}i"
+            }
+        }
+
+    val polar = "$magnitude \u2220 ${phase * 180 / Math.PI}\u00B0"
 
     operator fun plus(a: Complex): Complex {
         return Complex(this.real + a.real, this.imaginary + a.imaginary)
@@ -22,5 +35,9 @@ data class Complex(val real: Double, val imaginary: Double) {
         return Complex(
                 this.real * b.real - this.imaginary * b.imaginary,
                 this.real * b.imaginary + this.imaginary * b.real)
+    }
+
+    override fun toString(): String {
+        return rectangular
     }
 }
