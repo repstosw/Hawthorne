61e7327fab293a41556f0cb227e8a80b1f3a8511
jessebanks <jesse.banks@vendscreen.com>
2017-08-26 20:39:53 -0700

    MainActivity - start button does what it should

--- app/src/main/java/wtf/jessebanks/hawthorne/MainActivity.kt
+++ app/src/main/java/wtf/jessebanks/hawthorne/MainActivity.kt
@@ -4,44 +4,60 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import wtf.jessebanks.hawthorne.dsp.BufferQueue
 import wtf.jessebanks.hawthorne.dsp.ProcessingThread
 import wtf.jessebanks.hawthorne.dsp.RecordThread
-import wtf.jessebanks.hawthorne.dsp.fft
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
 
 class MainActivity : AppCompatActivity() {
 
-    private val audioQueue: BlockingQueue<ShortArray> = LinkedBlockingQueue()
+    private val audioQueue = BufferQueue(512)
 
     private val fftQueue: BlockingQueue<DoubleArray> = LinkedBlockingQueue(1)
-    private val recordThread = RecordThread(audioQueue)
-    private val processingThread = ProcessingThread(audioQueue, fftQueue)
+    private var recordThread = RecordThread(audioQueue)
+    private var processingThread = ProcessingThread(audioQueue, fftQueue)
     private val handler = Handler()
 
+    private var isRunning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recordThread.start()
-        processingThread.start()
+        plotView.scaley = 100
 
-        handler.post(plotUpdate)
+        activity_main.startButton.setOnClickListener { startProcessing()}
     }
 
     val plotUpdate: Runnable = object : Runnable {
         override fun run() {
             if (!fftQueue.isEmpty()) {
-                plotView.data = fftQueue.take()
+                var array = fftQueue.take()
+                array = array.copyOf(array.size / 2) // Nyquist
+                plotView.data = array
             }
-            handler.postDelayed(this, 250)
+            handler.postDelayed(this, 50)
         }
+    }
+
+
+    fun startProcessing() {
+
+        if (isRunning) return
+        recordThread = RecordThread(audioQueue)
+        processingThread = ProcessingThread(audioQueue, fftQueue)
+
+        recordThread.start()
+        processingThread.start()
+
+        isRunning = true
+        handler.post(plotUpdate)
     }
 
     override fun onResume() {
         super.onResume()
-
     }
 
     override fun onPause() {
@@ -49,6 +65,7 @@ class MainActivity : AppCompatActivity() {
         processingThread.exit = true
 
         handler.removeCallbacks(null)
+        isRunning = false
         super.onPause()
     }
 }
